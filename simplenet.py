1b3fe4d44f105d8fa598ebe481f828aac1c36985
Hugo Shaka <hugo+github@shaka.xyz>
2018-07-05 20:05:10 +0200

    Add process execution and simple bridge switch

--- simplenet.py
+++ simplenet.py
@@ -6,19 +6,20 @@ Can setup a dhcp server and a nat.
 
 Usage:
   simplenet switch [--name NAME] <interface>...
-  simplenet switch [--name NAME] --with-ip [--ip IP --range RANGE] [--dhcp] <interface>...
+  simplenet switch [--name NAME] --with-ip [(--ip IP --subnet SUBNET | --dhcp)] <interface>...
   simplenet nat <bridge> <destination>
   simplenet clean [--name NAME] [--except=<exception>]...
   simplenet -h | --help
   simplenet --version
 
 Options:
-    -h, --help          Print this help and exit.
-    -n, --name NAME    Name of the bridge [default: br0]
+    -d, --dhcp                        Get ip and subnet by dhcp
     -e, --except <exception>...       Do not touch this interface
-    -i, --ip IP        Machine ip on the bridge [default: 192.168.2.2]
-    -r, --range RANGE       Subnet of the bridge [default: 192.168.2.0/24]
-    -w, --with-ip           Connect the current machine to the bridged network
+    -h, --help                        Print this help and exit.
+    -i, --ip IP                       Machine ip on the bridge [default: 192.168.2.2]
+    -n, --name NAME                   Name of the bridge [default: br0]
+    -s, --subnet SUBNET               Subnet of the bridge [default: 192.168.2.0/24]
+    -w, --with-ip                     Connect the current machine to the bridged network
 
 Arguments:
     interface     Interface you want to add
@@ -28,8 +29,33 @@ Arguments:
 
 
 import logging
+import subprocess
 
 import docopt
+
+
+def unix_command(command, *, fatal=True):
+    """Takes a string representing the command, executes the command"""
+    log = logging.getLogger()
+    try:
+        _ = subprocess.check_output(
+            command,
+            stderr=subprocess.STDOUT,
+            shell=True,
+            timeout=3,
+            universal_newlines=True,
+        )
+    except subprocess.CalledProcessError as exc:
+        log.error(
+            """Command "%s" returned %d :
+               stdout: %r""",
+            command,
+            exc.returncode,
+            exc.output,
+        )
+        if fatal:
+            log.critical("Stopping program because a command failed")
+            raise
 
 
 def checks():
@@ -41,15 +67,17 @@ def checks():
     log.info("Running checks")
 
 
-def switch(name, interfaces):
+def switch(name, interfaces, with_ip=False, dhcp=False, ip=None, subnet=None):
     """ Setup a virtual switch on given ports.
     Can get tha machine an IP and setup a DHCP server
     """
     log = logging.getLogger()
-    log.info("Creating switch %s linking %r", name, interfaces)
-    # brctl addbr name
-    # brctl setfd 0
-    # brctl addif eth0, eth1
+    log.info("Creating switch %s", name)
+    unix_command("brctl addbr %s" % name)
+    unix_command("brctl setfd %s 0" % name)
+    for interface in interfaces:
+        log.info("Adding interface %s to bridge %s", interface, name)
+        unix_command("brctl addif %s %s" % (name, interface))
 
 
 def nat():
@@ -68,11 +96,11 @@ def dhcp():
     log.info("Creating DHCP server")
 
 
-def clean():
+def clean(name):
     """Clean our shit"""
     log = logging.getLogger()
-    log.info("Cleaning")
-    raise NotImplementedError("Clean not implemented")
+    log.info("Cleaning switch %s", name)
+    unix_command("brctl delbr %s" % name, fatal=False)
 
 
 def main():
@@ -95,16 +123,23 @@ def main():
     args = docopt.docopt(__doc__)
     log.debug(args)
 
-    if args["switch"]:
+    if args["clean"]:
+        clean(args["--name"])
+
+    elif args["switch"]:
         if args["--with-ip"]:
             raise NotImplementedError("--with-ip not available yet")
-        switch(args["--name"], args["<interface>"])
+        switch(
+            args["--name"],
+            args["<interface>"],
+            args["--with-ip"],
+            args["--dhcp"],
+            args["--ip"],
+            args["--subnet"],
+        )
 
     if args["nat"]:
         nat()
-
-    if args["clean"]:
-        clean()
 
 
 if __name__ == "__main__":
